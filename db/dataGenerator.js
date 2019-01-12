4f1d3ad094be742c40cb1d1cc4b07eec913bc211
Jessica <jessicarhodson@gmail.com>
2019-01-11 18:53:24 -0800

    Update restaurants with location category, outline schemas

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -3,27 +3,28 @@ const faker = require('faker');
 const fs = require('fs');
 
 // generate random restaurant info and format into csv entry
-const stringBuilder = (i, pictures) => {
+const stringBuilder = (i, pictures, places) => {
   let fakerInfo = {
     id: i,
-    name: faker.lorem.word(),
-    food: faker.lorem.words(),
-    wait_time: faker.random.number(60),
-    price: faker.random.number(15),
-    reviews: faker.random.number(2000),
-    stars: faker.random.number(100),
-    quality: faker.random.number(100),
-    timeliness: faker.random.number({
-      min: 60,
-      max: 100,
-    }),
     accuracy: faker.random.number({
       min: 70,
       max: 100,
     }),
-    username: faker.name.firstName(),
+    food: faker.lorem.words(),
+    location: places[faker.random.number(pictures.length - 1)],
+    name: faker.lorem.word(),
+    picture: pictures[faker.random.number(pictures.length - 1)],
+    price: faker.random.number(15),
+    quality: faker.random.number(100),
     review_text: faker.lorem.sentence(),
-    picture: pictures[faker.random.number(pictures.length - 1)]
+    reviews: faker.random.number(2000),
+    stars: faker.random.number(100),
+    timeliness: faker.random.number({
+      min: 60,
+      max: 100,
+    }),
+    username: faker.name.firstName(),
+    wait_time: faker.random.number(60),
   };
 
   let fakerArray = Object.values(fakerInfo);
@@ -35,22 +36,29 @@ const stringBuilder = (i, pictures) => {
 // write each restaurant entry to suggestions.csv, allowing for memory drain when necessary
 const dataGenerator = () => {
   const pictures = [];
-  const stream = fs.createWriteStream('suggestions.csv');
+  const places = [];
+  const stream = fs.createWriteStream('suggestions1.csv');
   let i = 1;
-  const max = 10000000;
+  const max = 100000;
 
-  stream.write('id,name,food,maxWaitTime,minWaitTime,reviewNo,stars,good,onTime,accurate,reviewUsername,review,picture,suggestions,bookmarked \n');
+  stream.write('id,name,food,location,wait_time,price,reviews,stars,quality,timeliness,accuracy,username,review_text,picture\n');
 
-  // generate array of 1000 random photo urls to choose from (for use in stringBuilder())
+  // generate array of 1000 random photo urls to choose from (for use in stringBuilder)
   for (let x = 0; x < 1001; x += 1) {
     pictures.push(faker.image.food(200, 200, true));
+  }
+
+  // generate array of 1000 random locations to choose from (for use in stringBuilder)
+
+  for (let x = 0; x < 1001; x += 1) {
+    places.push(faker.address.city());
   }
 
   const writer = () => {
     let result = true;
 
     while (i < max && result) {
-      result = stream.write(stringBuilder(i, pictures));
+      result = stream.write(stringBuilder(i, pictures, places));
       i += 1;
       if (i === 100000) {
         console.log(i);
