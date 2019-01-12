3596d519cf11d7eefb2341843a50533e009882a3
Jessica <jessicarhodson@gmail.com>
2019-01-12 14:36:00 -0800

    Change dataGenerator to seed a collection of suggestions to Cassandra

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -2,8 +2,28 @@ const faker = require('faker');
 
 const fs = require('fs');
 
+// generate set of random suggested restaurant ids
+const suggestionGenerator = (i) => {
+  let suggestions = '';
+  const num = faker.random.number({
+    min: 7,
+    max: 16
+  });
+  for (let x = 0; x < num; x += 1) {
+    const suggestedId = faker.random.number({min: 1, max: 999999 });
+    if (suggestedId !== i) {
+      if (x === 0) {
+        suggestions += `${suggestedId}`;  
+      } else {
+        suggestions += `, ${suggestedId}`;
+      }
+    }
+  }
+  return suggestions;
+}
+
 // generate random restaurant info and format into csv entry
-const stringBuilder = (i, pictures, places) => {
+const stringBuilder = (i, pictures) => {
   let fakerInfo = {
     id: i,
     accuracy: faker.random.number({
@@ -11,7 +31,6 @@ const stringBuilder = (i, pictures, places) => {
       max: 100,
     }),
     food: faker.lorem.words(),
-    location: places[faker.random.number(pictures.length - 1)],
     name: faker.lorem.word(),
     picture: pictures[faker.random.number(pictures.length - 1)],
     price: faker.random.number(15),
@@ -19,6 +38,7 @@ const stringBuilder = (i, pictures, places) => {
     review_text: faker.lorem.sentence(),
     reviews: faker.random.number(2000),
     stars: faker.random.number(100),
+    suggested: `{${suggestionGenerator(i)}}`,
     timeliness: faker.random.number({
       min: 60,
       max: 100,
@@ -28,7 +48,7 @@ const stringBuilder = (i, pictures, places) => {
   };
 
   let fakerArray = Object.values(fakerInfo);
-  let csvString = fakerArray.join(',');
+  let csvString = fakerArray.join('-');
 
   return `${csvString}\n`;
 };
@@ -39,19 +59,13 @@ const dataGenerator = () => {
   const places = [];
   const stream = fs.createWriteStream('suggestions1.csv');
   let i = 1;
-  const max = 100000;
+  const max = 1000000;
 
-  stream.write('id,name,food,location,wait_time,price,reviews,stars,quality,timeliness,accuracy,username,review_text,picture\n');
+  stream.write('id-name-food-wait_time-price-reviews-stars-suggested-quality-timeliness-accuracy-username-review_text-picture\n');
 
   // generate array of 1000 random photo urls to choose from (for use in stringBuilder)
   for (let x = 0; x < 1001; x += 1) {
     pictures.push(faker.image.food(200, 200, true));
-  }
-
-  // generate array of 1000 random locations to choose from (for use in stringBuilder)
-
-  for (let x = 0; x < 1001; x += 1) {
-    places.push(faker.address.city());
   }
 
   const writer = () => {
@@ -82,7 +96,7 @@ const dataGenerator = () => {
         console.log(i);
       }
     }
-    
+
     if (i < max) {
       stream.once('drain', writer);
     }
